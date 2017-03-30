00b6d6e0442adc0d2e29b1193c863342a0cb9ee4
coderevolutionary <coderevolutionary@users.noreply.github.com>
2017-03-30 01:20:05 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,6 @@
 angular.module('app', ['ngRoute', 'btford.markdown'])
    .config(['$routeProvider', function ($routeProvider) {
 
-      $routeProvider
-         .when('/', {
-            templateUrl: 'pages/about/about.html'
-         })
-         .otherwise({
-            redirectTo: '/'
-         });
-      $locationProvider.html5Mode(true);
    }]).run(function ($rootScope) {
-   $rootScope.$on('$locationChangeSuccess', function () {
-      $(document).scrollTop(0);
    });
 });
