4da11059484bb59fcbba951d76afb9f38d45f98d
- <->
2017-04-02 20:07:11 +0300

    href fix

--- vendors/markdown.js
+++ vendors/markdown.js
@@ -6,33 +6,45 @@
 
 'use strict';
 
-angular.module('btford.markdown', ['ngSanitize']).
-  provider('markdownConverter', function () {
-    var opts = {};
-    return {
+angular.module('btford.markdown', ['ngSanitize']).provider('markdownConverter', function () {
+   var opts = {};
+   return {
       config: function (newOpts) {
-        opts = newOpts;
+         opts = newOpts;
       },
       $get: function () {
-        return new showdown.Converter(opts);
+         return new showdown.Converter(opts);
       }
-    };
-  }).
-  directive('btfMarkdown', ['$sanitize', 'markdownConverter', function ($sanitize, markdownConverter) {
-    return {
+   };
+}).directive('btfMarkdown', ['$sanitize', 'markdownConverter', function ($sanitize, markdownConverter) {
+   return {
       restrict: 'AE',
       link: function (scope, element, attrs) {
-        if (attrs.btfMarkdown) {
-          scope.$watch(attrs.btfMarkdown, function (newVal) {
-            var html = newVal ? $sanitize(markdownConverter.makeHtml(newVal)) : '';
+         if (attrs.btfMarkdown) {
+            scope.$watch(attrs.btfMarkdown, function (newVal) {
+               var html = newVal ? $sanitize(markdownConverter.makeHtml(newVal)) : '';
+               element.html(html);
+               angular.forEach(element.find('a'), function (item) {
+                  if (item.href[0] != '/') {
+                     item.attr('target', '_blank');
+                  }
+                  else {
+                     item.href = '#!' + item.href;
+                  }
+               });
+            });
+         } else {
+            var html = $sanitize(markdownConverter.makeHtml(element.text()));
             element.html(html);
-            element.find('a').attr('target', '_blank');
-          });
-        } else {
-          var html = $sanitize(markdownConverter.makeHtml(element.text()));
-          element.html(html);
-          element.find('a').attr('target', '_blank');
-        }
+            angular.forEach(element.find('a'), function (item) {
+               if (item.href[0] != '/') {
+                  item.attr('target', '_blank');
+               }
+               else {
+                  item.href = '#!' + item.href;
+               }
+            });
+         }
       }
-    };
-  }]);
+   };
+}]);
